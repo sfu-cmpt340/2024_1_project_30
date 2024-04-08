2aab62d2fa95b4d04549d9be45a537aad28b4bdc
Chau Pham <71118130+Lepre-CHAU-n@users.noreply.github.com>
2024-04-07 23:48:35 -0700

    Update q3.m

--- Old_code/q3.m
+++ Old_code/q3.m
@@ -1,6 +1,6 @@
-# Creator: Evan Mangat
-# Input: Just change path directories to the ones where data is stored
-# Output: Visualizations and Graphs
+% Creator: Evan Mangat
+% Input: Just change path directories to the ones where data is stored
+% Output: Visualizations and Graphs
 
 % Load images
 img1 = imread('C:\Users\evanm\Documents\Homework\CMPT 340\Project\Blood_Cancer\Blood_Cancer\Sample_2.tiff');
